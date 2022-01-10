896451ea1cc0b918ff51425702a24a77b6600396
stackswithans <stexor12@gmail.com>
2022-01-11 00:48:02 +0100

    Update .gitignore

--- dist/main.d.ts
+++ dist/main.d.ts
@@ -1,3 +1,3 @@
-import { CellaStore, storeFromFile, storeFromObject } from "./store";
+import { Store, storeFromFile, storeFromObject } from "./store";
 import { op } from "./query";
-export { CellaStore, storeFromFile, storeFromObject, op };
+export { Store, storeFromFile, storeFromObject, op };
--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.op = exports.storeFromObject = exports.storeFromFile = exports.CellaStore = void 0;
+exports.op = exports.storeFromObject = exports.storeFromFile = exports.Store = void 0;
 const store_1 = require("./store");
-Object.defineProperty(exports, "CellaStore", { enumerable: true, get: function () { return store_1.CellaStore; } });
+Object.defineProperty(exports, "Store", { enumerable: true, get: function () { return store_1.Store; } });
 Object.defineProperty(exports, "storeFromFile", { enumerable: true, get: function () { return store_1.storeFromFile; } });
 Object.defineProperty(exports, "storeFromObject", { enumerable: true, get: function () { return store_1.storeFromObject; } });
 const query_1 = require("./query");
