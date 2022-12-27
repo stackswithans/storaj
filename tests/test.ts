70466f5a7b5ca1104979538e114fb7d77a6b0042
Marco Pitra <59171412+git-marcopitra@users.noreply.github.com>
2022-12-27 01:50:36 -0800

    Create-folder-recursive (#6)
    * :safety_vest: fix: recursive subpath tree
    
    * :safety_vest: fix: recursive subpath tree
    
    Co-authored-by: Sténio Jacinto <stexor12@gmail.com>

--- tests/test.ts
+++ tests/test.ts
@@ -1,3 +1,4 @@
+import { dirname } from 'path';
 import {
     itemHasProp,
     validateSerializedItem,
@@ -200,7 +201,7 @@ runTests(
     }),
 
     test("Test Store persist saves file if path given", async () => {
-        const filePath = "db/test.json";
+        const filePath = "storaj/recursive/path/test.json";
         const store = new Store(filePath);
         const testCol = store.collections("test");
         await testCol.insert({ message: "message1" }, 1);
@@ -212,6 +213,7 @@ runTests(
 
         //Clean up opened file
         await unlink(filePath);
+        rmSync(dirname(filePath).split('/')[0], { recursive: true });
         rmdirSync('db');
     }),
 
