7a7f5e9682906137653f7a2a3c6ab0e1fed598bf
Sténio Jacinto <stexor12@gmail.com>
2024-09-29 08:29:25 +0100

    :wrench: chore: update deps and build configs (#11)

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-export { Store } from "./store";
-export type { Collection } from "./store";
-export type { Query } from "./query";
-export * as QOp from "./qoperators";
+export { Store } from "./store.js";
+export type { Collection } from "./store.js";
+export type { Query } from "./query.js";
+export * as QOp from "./qoperators.js";
